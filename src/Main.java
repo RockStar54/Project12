a57f000d58dd2a6d0ae8e4911f2f4aa85139c730
Rockstar54 <rodin54@mail.ru>
2023-07-13 20:45:10 +0300

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 4, 5, 6};
+        int min = array[0];
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
